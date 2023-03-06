eea6d3c22f1ca3e286962cfcdf4047e3631f7b26
theopen4 <kielze86@gmail.com>
2023-03-06 22:29:31 +0100

    task

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const todo =  await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-const todoReponse = await todo.json()
+const todoReponse = await todo.json();
 
-console.log(todoReponse)
+function generatePage(todoList){
+    const sectionData = document.querySelector('main')
+    for(let i = 0; i < todoList.length; i++){
+        const todoList = todoReponse[i]
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.name = todoList.id
+        const label = document.createElement('label');
+        label.for = todoList.id 
+        sectionData.appendChild(input);
+    }
 
+}
+ 
 
 
