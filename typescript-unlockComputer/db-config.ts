3002953bda6b1f26d3aa32480a28755616105849
theopen4 <kielze86@gmail.com>
2023-03-28 20:27:44 +0100

    init models and routes api

---
+++ typescript-unlockComputer/db-config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+const app = express()
+app.use(express.json())
+app.use('/things',thingRouter)
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`)
+} )
